42f1a4c28f95cd2a24c41a032a990165ad7b1271
tringuyen <tringuyen@pingidentity.com>
2022-08-26 18:37:11 +0700

    Initial Commit

---
+++ COSC2659_Assignment2_s3726096/COSC2659_Assignment2_s3726096App.swift
@@ -0,0 +1,17 @@
+//
+//  COSC2659_Assignment2_s3726096App.swift
+//  COSC2659_Assignment2_s3726096
+//
+//  Created by Tri Nguyen on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct COSC2659_Assignment2_s3726096App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COSC2659_Assignment2_s3726096/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COSC2659_Assignment2_s3726096
+//
+//  Created by Tri Nguyen on 26/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
